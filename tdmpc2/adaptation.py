bd5e923ea231d034cde4c68b86b22c033501bf41
Dong Ho Kang <dongho@utexas.edu>
2024-04-24 17:54:48 -0500

    wip

---
+++ tdmpc2/adaptation.py
@@ -0,0 +1,194 @@
+import os
+import wandb
+
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+
+
+os.environ["MUJOCO_GL"] = "egl"
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import hydra
+import imageio
+import numpy as np
+import torch
+import torch.nn as nn
+from termcolor import colored
+from omegaconf import OmegaConf
+
+# Added Layers import
+from common import layers
+
+from common.parser import parse_cfg
+from common.seed import set_seed
+from envs import make_env
+from tdmpc2 import TDMPC2
+
+torch.backends.cudnn.benchmark = True
+
+
+@hydra.main(config_name="config", config_path=".")
+def train(cfg: dict):
+    """
+    Script for training the adaptation module for a single-task / multi-task TD-MPC2 checkpoint.
+
+    Most relevant args:
+            `task`: task name (or mt30/mt80 for multi-task evaluation)
+            `model_size`: model size, must be one of `[1, 5, 19, 48, 317]` (default: 5)
+            `checkpoint`: path to model checkpoint to load
+            `eval_episodes`: number of episodes to evaluate on per task (default: 10)
+            `save_video`: whether to save a video of the evaluation (default: True)
+            `seed`: random seed (default: 1)
+
+    See config.yaml for a full list of args.
+
+    Example usage:
+    ````
+            $ python evaluate.py task=mt80 model_size=48 checkpoint=/path/to/mt80-48M.pt
+            $ python evaluate.py task=mt30 model_size=317 checkpoint=/path/to/mt30-317M.pt
+            $ python evaluate.py task=dog-run checkpoint=/path/to/dog-1.pt save_video=true
+    ```
+    """
+    project_name = cfg.get("wandb_project", "none")
+    entity_name = cfg.get("wandb_entity", "none")
+    
+    wandb.init(
+            project=project_name,
+            entity=entity_name,
+            config=OmegaConf.to_container(cfg, resolve=True),
+            name="Adaptation test_longer_run",
+            monitor_gym=True,
+            save_code=True,
+        )
+    
+    assert torch.cuda.is_available()
+    assert cfg.adapt_episodes > 0, "Must create adaptation training for at least 1 episode."
+    cfg = parse_cfg(cfg)
+    set_seed(cfg.seed)
+    print(colored(f"Task: {cfg.task}", "blue", attrs=["bold"]))
+    print(
+        colored(
+            f'Model size: {cfg.get("model_size", "default")}', "blue", attrs=["bold"]
+        )
+    )
+    print(colored(f"Checkpoint: {cfg.checkpoint}", "blue", attrs=["bold"]))
+    if not cfg.multitask and ("mt80" in cfg.checkpoint or "mt30" in cfg.checkpoint):
+        print(
+            colored(
+                "Warning: single-task evaluation of multi-task models is not currently supported.",
+                "red",
+                attrs=["bold"],
+            )
+        )
+        print(
+            colored(
+                "To evaluate a multi-task model, use task=mt80 or task=mt30.",
+                "red",
+                attrs=["bold"],
+            )
+        )
+
+    # Make environment
+    env = make_env(cfg)
+
+    # Load agent
+    agent = TDMPC2(cfg)
+    assert os.path.exists(
+        cfg.checkpoint
+    ), f"Checkpoint {cfg.checkpoint} not found! Must be a valid filepath."
+    agent.load(cfg.checkpoint)
+
+    tasks = cfg.tasks if cfg.multitask else [cfg.task]
+    for task_idx, task in enumerate(tasks):
+        if not cfg.multitask:
+            task_idx = None
+
+        # TODO: Replace with configuration variable
+        obs_shape = 21
+        # TODO: Create config variable for the priviliged size
+        priv_size = 4
+        # TODO: Change history to be defined in the config file
+        history_length = 20
+        # Create the encoder
+        adapt_enc = create_encoder(cfg, obs_shape-priv_size, history_length)
+
+        optimizer = torch.optim.Adam(adapt_enc.parameters())
+        for i in range(cfg.adapt_episodes):
+            
+            obs, done, ep_reward, t = env.reset(task_idx=task_idx), False, 0, 0
+            
+            # Define history of obs
+            # TODO: Delete obs in the array over time as more come in
+            obs_ep = []
+            obs_reg = obs[0:-priv_size]
+            obs_ep.append(obs_reg)
+            epoch_loss = 0.0
+            batches = 0
+
+            while not done:
+                action = agent.act(obs, t0=t == 0, task=task_idx)
+                obs, reward, done, info = env.step(action)
+                #print(obs)
+                ep_reward += reward
+                
+                
+                # NOTE: new code added for adaptation module
+                # Remove the priviliged information
+                obs_reg = obs[0:-priv_size]
+                
+                obs_ep.append(obs_reg)
+                obs_flat = []
+                optimizer.zero_grad()
+                if (len(obs_ep) > history_length):
+                    obs_flat = torch.cat(obs_ep[-history_length:], dim =0)
+                else:
+                    obs_flat = torch.cat([torch.zeros_like(obs_ep[0])] * (history_length - len(obs_ep)) + obs_ep, dim =0)
+                new_z = adapt_enc(obs_flat).to(agent.device)
+                old_z = obtain_z (agent, obs, task_idx).to(agent.device)
+                loss = nn.MSELoss()(old_z, new_z)  # Calculate MSE loss
+                epoch_loss += loss.item()
+                batches += 1
+                loss.backward()  # Backpropagation
+                optimizer.step()  # Optimization step
+                t += 1
+            # Calculate average loss over the epoch
+            avg_epoch_loss = epoch_loss / batches
+
+            # Log average loss with wandb
+            wandb.log({"loss": avg_epoch_loss})
+            # Print average loss
+            print(f'Epoch [{i + 1}/{cfg.adapt_episodes}], Average Loss: {avg_epoch_loss:.6f}')
+            # TODO: Make a configuration file variable for the number of times the weights are saved
+            if (i + 1) % 10 == 0:
+                # Save encoder model checkpoint
+                encoder_checkpoint_path = os.path.join(wandb.run.dir, f"encoder_checkpoint_{i+1}.pt")
+                torch.save(adapt_enc.state_dict(), encoder_checkpoint_path)
+                print(encoder_checkpoint_path)
+        # Finish wandb run
+        wandb.finish()
+
+
+def obtain_z (agent, obs, task_idx):
+    task_t = task_idx
+    obs_t = obs.to(agent.device, non_blocking=True)
+    if task_t is not None:
+        task_t = torch.tensor([task_t], device=agent.device)
+    z_0 = agent.model.encode(obs_t, task_t)
+    return z_0.detach() # Teacher Z, detach so doesn't train
+
+def create_encoder (cfg, obs_shape, history_length):
+    #print( cfg.latent_dim)
+    #print(cfg.enc_dim)
+    # Changed encoder dim to be 1 hidden layers
+    enc_mlp = layers.mlp(
+                torch.prod(torch.tensor(obs_shape))*history_length + cfg.task_dim,
+                max(2 - 1, 1) * [cfg.enc_dim],
+                cfg.latent_dim,
+                act=layers.SimNorm(cfg),
+            )
+    return enc_mlp
+
+
+if __name__ == "__main__":
+    train()
---
+++ tdmpc2/evaluate_adapt.py
@@ -0,0 +1,216 @@
+# NOTE: This script is based on the evaluate.py script provided by TDMPC
+
+
+import os
+import gc
+
+os.environ["MUJOCO_GL"] = "egl"
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import hydra
+import imageio
+import numpy as np
+import torch
+from termcolor import colored
+
+from common.parser import parse_cfg
+from common.seed import set_seed
+from envs import make_env
+from tdmpc2 import TDMPC2
+
+torch.backends.cudnn.benchmark = True
+
+torch.cuda.memory_summary(device=None, abbreviated=False)
+@hydra.main(config_name="config", config_path=".")
+def evaluate(cfg: dict):
+    """
+    Script for evaluating a single-task / multi-task TD-MPC2 checkpoint.
+
+    Most relevant args:
+            `task`: task name (or mt30/mt80 for multi-task evaluation)
+            `model_size`: model size, must be one of `[1, 5, 19, 48, 317]` (default: 5)
+            `checkpoint`: path to model checkpoint to load
+            `eval_episodes`: number of episodes to evaluate on per task (default: 10)
+            `save_video`: whether to save a video of the evaluation (default: True)
+            `seed`: random seed (default: 1)
+
+    See config.yaml for a full list of args.
+
+    Example usage:
+    ````
+            $ python evaluate.py task=mt80 model_size=48 checkpoint=/path/to/mt80-48M.pt
+            $ python evaluate.py task=mt30 model_size=317 checkpoint=/path/to/mt30-317M.pt
+            $ python evaluate.py task=dog-run checkpoint=/path/to/dog-1.pt save_video=true
+    ```
+    """
+    # import wandb
+    # from omegaconf import OmegaConf
+    # project_name = cfg.get("wandb_project", "none")
+    # entity_name = cfg.get("wandb_entity", "none")
+    
+    # wandb.init(
+    #         project=project_name,
+    #         entity=entity_name,
+    #         config=OmegaConf.to_container(cfg, resolve=True),
+    #         name="Eval Adaptation",
+    #         monitor_gym=True,
+    #         save_code=True,
+    #     )
+    
+    
+    assert torch.cuda.is_available()
+    assert cfg.eval_episodes > 0, "Must evaluate at least 1 episode."
+    cfg = parse_cfg(cfg)
+    set_seed(cfg.seed)
+    print(colored(f"Task: {cfg.task}", "blue", attrs=["bold"]))
+    print(
+        colored(
+            f'Model size: {cfg.get("model_size", "default")}', "blue", attrs=["bold"]
+        )
+    )
+    print(colored(f"Checkpoint: {cfg.checkpoint}", "blue", attrs=["bold"]))
+    if not cfg.multitask and ("mt80" in cfg.checkpoint or "mt30" in cfg.checkpoint):
+        print(
+            colored(
+                "Warning: single-task evaluation of multi-task models is not currently supported.",
+                "red",
+                attrs=["bold"],
+            )
+        )
+        print(
+            colored(
+                "To evaluate a multi-task model, use task=mt80 or task=mt30.",
+                "red",
+                attrs=["bold"],
+            )
+        )
+
+    # Make environment
+    env = make_env(cfg)
+
+    # Load agent
+    agent = TDMPC2(cfg)
+    assert os.path.exists(
+        cfg.checkpoint
+    ), f"Checkpoint {cfg.checkpoint} not found! Must be a valid filepath."
+   
+    # Loads previous model plus the new encoder
+    agent.load(cfg.checkpoint, "/home/dk/encoder_checkpoint_3240.pt")
+    #agent.load(cfg.checkpoint)
+    # TODO: Change to configuration file variable
+    priv_size = 4
+    # TODO: Change history to be defined in the config file
+    history_length = 20
+
+    # Evaluate
+    if cfg.multitask:
+        print(
+            colored(
+                f"Evaluating agent on {len(cfg.tasks)} tasks:", "yellow", attrs=["bold"]
+            )
+        )
+    else:
+        print(colored(f"Evaluating agent on {cfg.task}:", "yellow", attrs=["bold"]))
+    if cfg.save_video:
+        video_dir = os.path.join(cfg.work_dir, "videos")
+        os.makedirs(video_dir, exist_ok=True)
+    scores = []
+    tasks = cfg.tasks if cfg.multitask else [cfg.task]
+    with torch.no_grad():
+        for task_idx, task in enumerate(tasks):
+            if not cfg.multitask:
+                task_idx = None
+            ep_rewards, ep_successes = [], []
+            for i in range(10): # TODO: Add a parameter in the config for the number of epochs
+                obs, done, ep_reward, t = env.reset(task_idx=task_idx), False, 0, 0
+
+                obs_reg = obs[0:-priv_size]
+                obs_ep = []
+                obs_ep.append(obs_reg)
+
+                if cfg.save_video:
+                    frames = [env.render()]
+                while not done:
+                    obs_flat = []
+                    if(len(obs_ep) < history_length):
+                        obs_flat = torch.cat([torch.zeros_like(obs_ep[0])] * (history_length - len(obs_ep)) + obs_ep, dim =0)   
+                    else:
+                        obs_flat = torch.cat(obs_ep[-history_length:], dim =0)
+                    
+                    #obs_flat = obs
+                    
+                    action = agent.act(obs_flat, t0=t == 0, task=task_idx)
+                    
+                    
+                    
+                    obs, reward, done, info = env.step(action)
+                    ep_reward += reward
+
+                    obs_reg = obs[0:-priv_size]
+                    obs_ep.append(obs_reg)
+
+                    if cfg.save_video:
+                        update_markers(agent, env, cfg, obs_flat, t == 0, task_idx)
+                        frames.append(env.render())
+
+                    t += 1
+                obs_ep = []
+                torch.cuda.empty_cache()    
+                gc.collect()
+                ep_rewards.append(ep_reward)
+                ep_successes.append(info["success"])
+                print(info["success"])
+                if cfg.save_video:
+                    imageio.mimsave(
+                        os.path.join(video_dir, f"{task}-{i}.mp4"), frames, fps=15
+                    )
+            ep_rewards = np.mean(ep_rewards)
+            ep_successes = np.mean(ep_successes)
+            if cfg.multitask:
+                scores.append(
+                    ep_successes * 100 if task.startswith("mw-") else ep_rewards / 10
+                )
+            print(
+                colored(
+                    f"  {task:<22}" f"\tR: {ep_rewards:.01f}  " f"\tS: {ep_successes:.02f}",
+                    "yellow",
+                )
+            )
+        if cfg.multitask:
+            print(
+                colored(
+                    f"Normalized score: {np.mean(scores):.02f}", "yellow", attrs=["bold"]
+                )
+            )
+
+
+def update_markers(agent, env, cfg, obs, t0, task_idx):
+    task_t = task_idx
+    obs_t = obs.to(agent.device, non_blocking=True)
+    if task_t is not None:
+        task_t = torch.tensor([task_t], device=agent.device)
+
+    z_0 = agent.model.encode(obs_t, task_t)
+    z_s = torch.empty(
+        cfg.num_pi_trajs + 1,
+        cfg.horizon + 1,
+        cfg.latent_dim,
+        device=agent.device,
+    )
+
+    # sample TDMPC planning
+    plan = agent.plan(z_0, t0=t0, eval_mode=True, task=task_idx)
+    z_s[0, 0] = z_0
+    for i, action in enumerate(plan):
+        z_s[0, i + 1] = agent.model.next(z_s[0, i], action, task_idx)
+
+    # sample all policy trajectories
+    z_s[1:] = agent.sample_trajectories(z_0, task=task_idx)
+
+    env.unwrapped.update_trajectories(agent.model.decode(z_s).detach().cpu())
+
+
+if __name__ == "__main__":
+    evaluate()
